0d2c4c834cca509d11f79bde7f85f9d363f2bac0
zikeyboy <108185057+zikeyboy@users.noreply.github.com>
2022-06-25 15:05:15 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-sudo yum -y update
-sudo yum -y install python2-pip
+sudo apt -y update
+sudo apt -y install python2-pip
 sudo pip install ansible==2.8.7
 
 ansible-playbook --connection=local -i "localhost," ansible/prepare-server.yml
